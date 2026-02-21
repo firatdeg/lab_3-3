bde9e6d2ac46a891197f456df1591f5b250263af
mdegir <degirmencioglu@studiumdigitale.uni-frankfurt.de>
2026-02-22 00:53:22 +0100

    some random things to verify something in github

--- source/utilities.cpp
+++ source/utilities.cpp
@@ -1,1 +1,8 @@
 #include "utilities.h"
+void gpu_max_reduction(const double* d_in, double* d_out, std::size_t size) {
+    // Kernel launch parameters
+    dim3 blockSize(256);
+    dim3 gridSize((size + blockSize.x - 1) / blockSize.x);
+
+    max_reduction_kernel<<<gridSize, blockSize>>>(d_in, d_out, size);
+}
